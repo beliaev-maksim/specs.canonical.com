c09eb32fc5294380849786c574f5783ef508f74e
Dampolo03 <shobowaledamilolapaul@gmail.com>
2022-10-06 09:30:11 +0100

    Extract service account info to settings

--- webapp/spec.py
+++ webapp/spec.py
@@ -1,5 +1,4 @@
 import io
-import os
 
 from apiclient.http import MediaIoBaseDownload
 from bs4 import BeautifulSoup
@@ -10,32 +9,15 @@ from dateutil.parser import parse
 
 from flask import abort
 
+from settings import SERVICE_ACCOUNT_INFO
+
 
 class GoogleDrive:
     def __init__(self):
-        PRIVATE_KEY_ID = os.getenv("PRIVATE_KEY_ID")
-        PRIVATE_KEY = os.getenv("PRIVATE_KEY")
-
         scopes = [
             "https://www.googleapis.com/auth/drive.readonly",
         ]
-        service_account_info = {
-            "type": "service_account",
-            "project_id": "roadmap-270011",
-            "private_key_id": PRIVATE_KEY_ID,
-            "private_key": PRIVATE_KEY,
-            "client_email": "specs-reader@roadmap-270011.iam.gserviceaccount.com",
-            "client_id": "112404606310881291739",
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url": (
-                "https://www.googleapis.com/oauth2/v1/certs"
-            ),
-            "client_x509_cert_url": (
-                "https://www.googleapis.com/robot/v1/metadata"
-                "/x509/specs-reader%40roadmap-270011.iam.gserviceaccount.com"
-            ),
-        }
+        service_account_info = SERVICE_ACCOUNT_INFO
         credentials = service_account.Credentials.from_service_account_info(
             service_account_info, scopes=scopes
         )
