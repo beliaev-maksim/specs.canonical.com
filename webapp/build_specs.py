30d6b1724f41f3293e51aaf16035e54f2c9b8c21
Maksim Beliaev <beliaev.m.s@gmail.com>
2023-12-07 09:07:31 +0100

    Add sorting by creation date option (#164)
    * added possibility to sort by creation date
    * add accuracy to the spec time

--- webapp/build_specs.py
+++ webapp/build_specs.py
@@ -11,7 +11,7 @@ def get_value_row(row, type):
             if "formattedValue" in row:
                 return datetime.strptime(
                     row["formattedValue"], "%Y-%m-%dT%H:%M:%S.%fZ"
-                ).strftime("%d %b %Y")
+                ).strftime("%d %b %Y %H:%M")
         elif "userEnteredValue" in row:
             if "stringValue" in row["userEnteredValue"]:
                 return type(row["userEnteredValue"]["stringValue"])
@@ -19,6 +19,7 @@ def get_value_row(row, type):
                 return type(row["userEnteredValue"]["numberValue"])
 
     return ""
+
 
 def index_in_list(a_list, index):
     return index < len(a_list)
